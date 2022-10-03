54f8e559e52c9aea116490f26764edb12a65457e
mlarmet <maxencelarmet@orange.fr>
2022-10-03 21:05:51 +0200

    update: add new project

--- js/slide.js
+++ js/slide.js
@@ -1,20 +1,12 @@
 let slidesData = [
 	{
-		title: "Meteo api",
-		text: ["Conception et réalisation d'un site web pour <em>traiter</em> des données météo récupérées à partir d'une <em>API</em>.", "Outils : HTML, CSS, JS (vanilla)", "Acquis : Découverte API et appel AJAX"],
-
-		link: "https://mlarmet.github.io/meteo-api",
-
-		img: {
-			name: "meteo-api-full.jpeg",
-			alt: "site meteo api",
-			title: "Site météo",
-		},
-	},
-	{
 		title: "Pizza8000",
-		text: ["Conception et réalisation d'un site web vitrine pour une pizzeria local \
-        avec <em>personnalisation</em> et <em>sessions admin</em>.", "Outils : PHP (Framework Codeigniter)", "Acquis : Modèle MVC, Première expérience professionnelle"],
+		text: [
+			"Conception et réalisation d'un site web vitrine pour une pizzeria local \
+        avec <em>personnalisation</em> et <em>sessions admin</em>.",
+			"Outils : PHP (Framework Codeigniter)",
+			"Acquis : Modèle MVC, Première expérience professionnelle",
+		],
 
 		link: "https://www.pizza8000.fr",
 
@@ -25,22 +17,44 @@ let slidesData = [
 		},
 	},
 	{
-		title: "Jeux en Python",
-		text: ["Réalisation de plusieurs petits <em>jeux</em> en <em>python</em> (en mode\
-            texte) tel que : pendu, morpion", "Outils : Python (3.7) avec le logiciel Visual Studio Code", "Acquis : persévérance, autonomie"],
+		title: "Meteo api",
+		text: [
+			"Conception et réalisation d'un site web pour <em>traiter</em> des données météo récupérées à partir d'une <em>API</em>.",
+			"Outils : HTML, CSS, JS (vanilla)",
+			"Acquis : Découverte API et appel AJAX",
+		],
 
+		link: "https://mlarmet.github.io/meteo-api",
+
+		img: {
+			name: "meteo-api-full.jpeg",
+			alt: "site meteo api",
+			title: "Site météo",
+		},
+	},
+	{
+		title: "Emplois du temps",
+		text: [
+			"Conception et réalisation d'une application web qui <em>récupère</em> et <em>affiche</em> un emplois du temps",
+			"Outils : React (18) et NodeJS (16)",
+			'Acquis : Traitement des données "ical", autonomie',
+		],
 		link: "",
 
 		img: {
-			name: "python_logo.png",
-			alt: "illustration projets python",
-			title: "Jeu en python",
+			name: "edt.jpeg",
+			alt: "site emplois du temps",
+			title: "Emplois du temps",
 		},
 	},
 	{
 		title: "Casse brique",
-		text: ["Conception et réalisation d'un <em>jeu vidéo 2D</em> pour le projet de fin \
-        d'année de la spécialité ISN du Baccalauréat.", "Outils : Python (3.6) avec le module pygame", "Acquis : gestion du temps, travail en équipe"],
+		text: [
+			"Conception et réalisation d'un <em>jeu vidéo 2D</em> pour le projet de fin \
+        d'année de la spécialité ISN du Baccalauréat.",
+			"Outils : Python (3.6) avec le module pygame",
+			"Acquis : gestion du temps, travail en équipe",
+		],
 
 		link: "https://github.com/mlarmet/casse-brique",
 
@@ -52,7 +66,11 @@ let slidesData = [
 	},
 	{
 		title: "Tic Tac Toe",
-		text: ["<em>Développement</em> d'une application <em> Android</em>. Jeu du tic tac toe.", "Outils : Java avec le logiciel Android Studio, XML", "Acquis : initiation à la création d'application, autonomie"],
+		text: [
+			"<em>Développement</em> d'une application <em> Android</em>. Jeu du tic tac toe.",
+			"Outils : Java avec le logiciel Android Studio, XML",
+			"Acquis : initiation à la création d'application, autonomie",
+		],
 
 		link: "",
 
